22ed758ad3a80638f560c52fd88cd1464ab1a3e1
Michael Fontecchio <mfonte3@live.spcollege.edu>
2018-03-23 21:47:58 -0400

    Full Commit for Assignment 2

---
+++ app/src/main/java/com/bignerdeanch/android/fitleaf/CustomerListActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdeanch.android.fitleaf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_list);
+    }
+}
